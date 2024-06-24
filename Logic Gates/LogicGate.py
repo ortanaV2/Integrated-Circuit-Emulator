61148959b79f154585c26e222d0261e44ae56aba
Ortana <126905156+ortanaV2@users.noreply.github.com>
2024-06-24 13:50:03 +0200

    Logic Gates Simulator Set

---
+++ Logic Gates/LogicGate.py
@@ -0,0 +1,61 @@
+class LogicGate:
+    def __init__(self) -> None:
+        self.returnType = bool
+
+    def returnInt(self): self.returnType = int
+    def returnBool(self): self.returnType = bool
+
+    def output(self, type_):
+        if type_ and self.returnType == bool: return True
+        if not type_ and self.returnType == bool: return False
+        if type_ and self.returnType == int: return 1
+        if not type_ and self.returnType == int: return 0
+
+    class And:
+        @staticmethod
+        def inp2(a: bool, b: bool) -> bool: return a and b
+        @staticmethod
+        def inp4(a: bool, b: bool, c: bool, d: bool) -> bool: return a and b and c and d
+
+    class Or:
+        @staticmethod
+        def inp2(a: bool, b: bool) -> bool: return a or b
+        @staticmethod
+        def inp4(a: bool, b: bool, c: bool, d: bool) -> bool: return a or b or c or d
+
+    class Not:
+        @staticmethod
+        def inp(a: bool) -> bool: return not a
+
+    class Nand:
+        @staticmethod
+        def inp2(a: bool, b: bool) -> bool: return not (a and b)
+        @staticmethod 
+        def inp4(a: bool, b: bool, c: bool, d: bool) -> bool: return not (a and b and c and d)
+
+    class Nor:
+        @staticmethod
+        def inp2(a: bool, b: bool) -> bool: return not (a or b)
+        @staticmethod
+        def inp4(a: bool, b: bool, c: bool, d: bool) -> bool: return not (a or b or c or d)
+
+    class Xor:
+        @staticmethod
+        def inp2(a: bool, b: bool) -> bool: return (a and not b) or (not a and b)
+
+    class Xnor:
+        @staticmethod
+        def inp2(a: bool, b: bool) -> bool: return not ((a and not b) or (not a and b))
+
+#Examples:
+gate = LogicGate()
+print(gate.output(LogicGate.Or.inp2(True, False)))
+print(gate.output(LogicGate.Not.inp(False)))
+print(gate.output(LogicGate.Nor.inp2(False, False)))
+print(gate.output(LogicGate.Xnor.inp2(False, False)))
+
+gate.returnInt()
+print(gate.output(LogicGate.Or.inp2(True, False)))
+print(gate.output(LogicGate.Not.inp(False)))
+print(gate.output(LogicGate.Nor.inp2(False, False)))
+print(gate.output(LogicGate.Xnor.inp2(False, False)))
